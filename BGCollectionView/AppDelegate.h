1042a847f8a95ee666170a82418afa9406550e49
yangshebing <shebing_yang@163.com>
2015-10-25 14:41:47 +0800

    Commit the project all file

---
+++ BGCollectionView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BGCollectionView
+//
+//  Created by 杨社兵 on 15/10/25.
+//  Copyright © 2015年 FAL. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BGCollectionView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BGCollectionView
+//
+//  Created by 杨社兵 on 15/10/25.
+//  Copyright © 2015年 FAL. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
